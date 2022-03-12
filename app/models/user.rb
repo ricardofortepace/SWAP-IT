147f089be1e2d470139bf243cd9a9542a8d81411
claudioferreira <claudioferreirajr@hotmail.com>
2022-03-12 15:52:52 -0300

    geocoding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,7 +10,4 @@ class User < ApplicationRecord
   has_one_attached :photo
   # has_many :messages, dependent: :destroy
   # has_many :matches, through: :stuffs, dependent: :destroy
-
-
-
 end
