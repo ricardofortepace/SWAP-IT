96927035748dd8b369bbc5f538306f72d0aa474a
claudioferreira <claudioferreirajr@hotmail.com>
2022-03-15 16:18:30 -0300

    range

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,13 +4,12 @@ class LikesController < ApplicationController
     # @user_near = User.near(params[:address], params[:range]) if params[:address].present?
     redirect_to new_stuff_path, notice: "you still need to add some stuff to swap." if current_user.stuffs.empty?
     redirect_to root_path, notice: "Sorry, but we dont have stuffs for you swap." if Stuff.where.not(user: current_user).empty?
-    # @swap_stuff = Stuff.near(current_user, current_user.range).where.not(user: current_user).sample
+    redirect_to root_path, notice: "Sorry, but we dont have stuffs in your range." if Stuff.near(current_user, current_user.range).empty?
+    @swap_stuff = Stuff.near(current_user, current_user.range).where.not(user: current_user).sample
 
-    @swap_stuff = Stuff.where.not(user: current_user).sample
+    # @swap_stuff = Stuff.where.not(user: current_user).sample
 
-    # dentro dos swap_stuff somente os que satisfazem o range preestabelecido
     @my_stuffs = current_user.stuffs
-
     @like = Like.new
   end
 
